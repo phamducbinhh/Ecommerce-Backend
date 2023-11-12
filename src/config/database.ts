d7ed2dc27ee5be23bc68bae1c5505b11a71a44a1
Your Name01~ <you@example.com>
2023-11-12 23:14:18 +0700

    Update Connect comment

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,17 +2,21 @@ const Sequelize = require('sequelize')
 const dotenv = require('dotenv')
 dotenv.config()
 
+// Lấy thông tin kết nối cơ sở dữ liệu từ biến môi trường
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME } = process.env
+// Khởi tạo đối tượng Sequelize để kết nối đến cơ sở dữ liệu MySQL
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   port: DB_PORT,
-  dialect: 'mysql',
-  timezone: '+07:00',
-  logging: false
+  dialect: 'mysql', // Sử dụng MySQL làm cơ sở dữ liệu
+  timezone: '+07:00', // Đặt múi giờ cho cơ sở dữ liệu, ở đây là UTC+7
+  logging: false // Tắt logging SQL queries để giảm tiêu tốn tài nguyên
 })
 
+// Hàm kết nối đến cơ sở dữ liệu
 const connectDatabase = async () => {
   try {
+    // Kiểm tra kết nối đến cơ sở dữ liệu
     await sequelize.authenticate()
     console.log('Database connected.')
   } catch (error) {
--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,21 @@ const { connectDatabase } = require('./config/database')
 dotenv.config()
 
 const app = express()
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors()) // Sử dụng middleware Cors để xử lý CORS
+app.use(bodyParser.json()) // Sử dụng middleware body-parser để xử lý dữ liệu JSON
+app.use(bodyParser.urlencoded({ extended: true })) // Sử dụng middleware body-parser để xử lý dữ liệu từ form
 
+// Hàm bắt đầu server
 const startServer = async (): Promise<void> => {
   try {
-    await connectDatabase()
+    await connectDatabase() // Kết nối đến cơ sở dữ liệu trước khi bắt đầu server
     app.listen(process.env.PORT || 8080, () => {
-      console.log(`App server listening on port: ${process.env.PORT}`)
+      console.log(`App server listening on port: ${process.env.PORT || 8080}`)
     })
   } catch (err) {
     console.error('App server error:', (err as Error).stack)
   }
 }
 
+// Gọi hàm startServer để bắt đầu server khi ứng dụng được chạy
 startServer()
