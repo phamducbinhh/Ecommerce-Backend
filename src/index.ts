52b217fa65847410eb8dfb6a44829b10699e4bc4
Your Name01~ <you@example.com>
2023-11-15 22:31:20 +0700

    Update Folder

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const { connectDatabase } = require('./utils/database')
+const { connectDatabase } = require('./config/database')
 dotenv.config()
 
 const app = express()
