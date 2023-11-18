791f48f76c11acddc524f1f5796008f2e5ccaa52
Your Name01~ <you@example.com>
2023-11-18 17:06:48 +0700

    Update Model

---
+++ src/models/index.ts
@@ -0,0 +1,8 @@
+const Products = require('./products.model.ts')
+const ProductsCatgory = require('./productsCategory.model')
+const User = require('./users.models')
+
+module.exports = {
+  Products,
+  ProductsCatgory
+}
