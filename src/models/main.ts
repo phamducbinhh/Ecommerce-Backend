03c2cf6e66be691f8eba0ec5edcc4ca0d9961861
phamducbinhh <phamducbinh1712000@gmail.com>
2023-11-29 16:25:22 +0700

    Update

--- src/models/main.ts
+++ src/models/main.ts
@@ -1,9 +1,5 @@
-const Products = require('./products.model.ts')
-const ProductsCatgory = require('./productsCategory.model')
 const User = require('./users.models')
 
 module.exports = {
-  Products,
-  ProductsCatgory,
   User
 }
